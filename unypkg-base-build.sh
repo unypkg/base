283435af38166b2c4c8845d0f29bcda17bced4a6
cyrayn <cyrayn@foundyn.com>
2024-01-08 11:02:00 +0100

    Update unypkg-base-build.sh

--- unypkg-base-build.sh
+++ unypkg-base-build.sh
@@ -860,7 +860,7 @@ cat >~/.bash_profile <<"EOF"
 exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
 EOF
 
-cat >~/.bashrc <<"EOF"
+cat >~/.profile <<"EOF"
 set +h
 umask 022
 UNY=/uny
@@ -873,14 +873,21 @@ CONFIG_SITE=$UNY/usr/share/config.site
 export UNY LC_ALL UNY_TGT PATH CONFIG_SITE
 MAKEFLAGS="-j$(nproc)"
 EOF
-
-# shellcheck source=/dev/null
-#source ~/.bash_profile
-# shellcheck source=/dev/null
-#source ~/.bashrc
 EOFUNY
 
 sudo -i -u uny bash <<"EOFUNY"
+set -vx
+set +h
+umask 022
+UNY=/uny
+LC_ALL=POSIX
+UNY_TGT=$(uname -m)-uny-linux-gnu
+PATH=/usr/bin
+if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
+PATH=$UNY/tools/bin:$PATH
+CONFIG_SITE=$UNY/usr/share/config.site
+export UNY LC_ALL UNY_TGT PATH CONFIG_SITE
+MAKEFLAGS="-j$(nproc)"
 
 ######################################################################################################################
 ######################################################################################################################
