9d4431237d071718944aa0c4ccfb07f8e9d39efe
michacassola <mich@casso.la>
2024-05-17 14:21:11 +0200

    gcc fixes

--- unypkg-base-build-stage2.sh
+++ unypkg-base-build-stage2.sh
@@ -281,7 +281,7 @@ ln -sfv /uny/pkg/"$pkgname"/"$pkgver"/share/zoneinfo/Europe/Berlin /etc/localtim
 cleanup_verbose_off_timing_end
 
 ######################################################################################################################
-### Prepare gcc-shared libs until replaced with gcc's below
+### Prepare gcc libs until replaced with gcc's below
 
 gcc_ver="$(echo /sources/gcc-* | grep -Eo "gcc-[^0-9]*(([0-9]+\.)*[0-9]+)" | sort -u | sed "s|gcc-||")"
 mkdir -pv /uny/pkg/gcc/"$gcc_ver"/lib
@@ -1092,7 +1092,7 @@ cleanup_verbose_off_timing_end
 
 ######################################################################################################################
 ### GCC
-pkgname="gcc-shared"
+pkgname="gcc"
 
 version_verbose_log_clean_unpack_cd
 get_env_var_values
@@ -1138,12 +1138,6 @@ su tester -c "PATH=$PATH make -k -j$(nproc) check"
 chown -R root:root .
 make install
 
-### Move bin, include and libexec directories to gcc folder
-#mkdir -pv /uny/pkg/gcc/$pkgver
-#mv -v /uny/pkg/$pkgname/$pkgver/bin /uny/pkg/gcc/$pkgver/bin
-#mv -v /uny/pkg/$pkgname/$pkgver/include /uny/pkg/gcc/$pkgver/include
-#mv -v /uny/pkg/$pkgname/$pkgver/libexec /uny/pkg/gcc/$pkgver/libexec
-
 ln -sv gcc /uny/pkg/gcc/"$pkgver"/bin/cc
 ln -svr /uny/pkg/gcc/"$pkgver"/bin/cpp /uny/pkg/"$pkgname"/"$pkgver"/lib
 ln -sfv /uny/pkg/gcc/"$pkgver"/libexec/gcc/"$(gcc -dumpmachine)"/"$pkgver"/liblto_plugin.so \
@@ -1155,11 +1149,6 @@ echo -n "/uny/pkg/gcc/$pkgver/include:/uny/pkg/gcc/$pkgver/include/c++/$pkgver" 
 ### End of individual build script
 
 add_to_paths_files
-
-### Also add gcc folders to paths files
-#pkgname="gcc"
-#add_to_paths_files
-
 dependencies_file_and_unset_vars
 cleanup_verbose_off_timing_end
 
