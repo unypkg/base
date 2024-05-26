ed238126ec42ccc48428711813639f6357444b2b
michacassola <mich@casso.la>
2024-05-26 18:47:44 +0200

    Update unypkg-create-repos.sh

--- unypkg-create-repos.sh
+++ unypkg-create-repos.sh
@@ -32,7 +32,7 @@ source /uny/git/unypkg/fn
 
 uny_auto_github_conf
 
-pkgrepos=("libexif" "libgd" "freetype" "cmake" "libarchive" "libuv" "libevent" "libzip" "imagemagick")
+pkgrepos=("yasm" "mariadb" "boost" "libaio" "xorg-libs")
 for pkgrepo in "${pkgrepos[@]}"; do
     pkgname="$pkgrepo"
     check_for_repo_and_create
