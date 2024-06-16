1b6d642c5deb61244b14446f74fb7afdec41e9b3
michacassola <mich@casso.la>
2024-06-16 14:14:19 +0200

    Update unypkg-create-repos.sh

--- unypkg-create-repos.sh
+++ unypkg-create-repos.sh
@@ -32,7 +32,7 @@ source /uny/git/unypkg/fn
 
 uny_auto_github_conf
 
-pkgrepos=("lmdb")
+pkgrepos=("libyaml" "ruby")
 for pkgrepo in "${pkgrepos[@]}"; do
     pkgname="$pkgrepo"
     check_for_repo_and_create
