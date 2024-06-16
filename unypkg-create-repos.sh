80d2b7d34da3d18008977ef8be0b5adfdbc61b9c
michacassola <mich@casso.la>
2024-06-16 13:34:54 +0200

    Update unypkg-create-repos.sh

--- unypkg-create-repos.sh
+++ unypkg-create-repos.sh
@@ -32,7 +32,7 @@ source /uny/git/unypkg/fn
 
 uny_auto_github_conf
 
-pkgrepos=("xorg" "fontconfig")
+pkgrepos=("lmdb")
 for pkgrepo in "${pkgrepos[@]}"; do
     pkgname="$pkgrepo"
     check_for_repo_and_create
