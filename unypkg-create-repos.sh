34edc20ecf57dffb3c0a9caab8a313b526d0ee3b
michacassola <mich@casso.la>
2024-07-15 16:49:49 +0200

    Update unypkg-create-repos.sh

--- unypkg-create-repos.sh
+++ unypkg-create-repos.sh
@@ -32,7 +32,7 @@ source /uny/git/unypkg/fn
 
 uny_auto_github_conf
 
-pkgrepos=("pcre")
+pkgrepos=("procps")
 for pkgrepo in "${pkgrepos[@]}"; do
     pkgname="$pkgrepo"
     check_for_repo_and_create
