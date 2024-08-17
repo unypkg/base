9686e0171e9472c817e7c5eca6a57b1b03dddef7
michacassola <mich@casso.la>
2024-08-17 15:09:01 +0200

    Update unypkg-create-repos.sh

--- unypkg-create-repos.sh
+++ unypkg-create-repos.sh
@@ -32,7 +32,7 @@ source /uny/git/unypkg/fn
 
 uny_auto_github_conf
 
-pkgrepos=("liburing")
+pkgrepos=("lz4")
 for pkgrepo in "${pkgrepos[@]}"; do
     pkgname="$pkgrepo"
     check_for_repo_and_create
