26446fb07fd38bddc469f98cb2692bcc59c7577d
michacassola <mich@casso.la>
2024-09-11 19:44:42 +0200

    Create git

--- unypkg-create-repos.sh
+++ unypkg-create-repos.sh
@@ -32,7 +32,7 @@ source /uny/git/unypkg/fn
 
 uny_auto_github_conf
 
-pkgrepos=("re2" "abseil-cpp")
+pkgrepos=("git")
 for pkgrepo in "${pkgrepos[@]}"; do
     pkgname="$pkgrepo"
     check_for_repo_and_create
