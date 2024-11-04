16c1f1d09246083398fe8b93913a96601ff26aed
michacassola <mich@casso.la>
2024-11-04 16:07:17 +0100

    Update unypkg-create-repos.sh

--- unypkg-create-repos.sh
+++ unypkg-create-repos.sh
@@ -32,7 +32,7 @@ source /uny/git/unypkg/fn
 
 uny_auto_github_conf
 
-pkgrepos=("git")
+pkgrepos=("sqlite" "postgresql")
 for pkgrepo in "${pkgrepos[@]}"; do
     pkgname="$pkgrepo"
     check_for_repo_and_create
