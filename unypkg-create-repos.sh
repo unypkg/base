ee74a350cbf2cf621b629fc1d42ff5464d08c6ac
Micha Cassola <mich@casso.la>
2025-02-04 17:11:52 +0100

    Update unypkg-create-repos.sh

--- unypkg-create-repos.sh
+++ unypkg-create-repos.sh
@@ -32,7 +32,7 @@ source /uny/pkg/unypkg/fn
 
 uny_auto_github_conf
 
-pkgrepos=("ghostscript")
+pkgrepos=("valkey")
 for pkgrepo in "${pkgrepos[@]}"; do
     pkgname="$pkgrepo"
     check_for_repo_and_create
